912d885f004fe3b381b13ed29057c2be23bd83c5
Станислав Микулич <C:\Users\Lz42\AppData\Roaming\The Bat!>
2024-05-16 11:02:57 +0300

    AUTO FROM work 16.05.2024 11:02:57,36

--- aud/views.py
+++ aud/views.py
@@ -6,6 +6,6 @@ def index(request):
     a = Aud.objects.order_by("name")
     return render(request, "aud/aud.html", context={"aud": a})
 def aud(request, id):
-    t=id
+    t = id
     a = Aud.objects.get(id=t)
     return render(request, "aud/audone.html", context={"a": a})
