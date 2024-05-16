32e10672a554b8ea3dcf7b7da7de04726c9c7521
stanislaumich <staslz42stas.com>
2024-05-16 07:34:14 +0300

    AUTO FROM HOME 16.05.2024  7:34:14,44

--- person/models.py
+++ person/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Person(models.Model):
-	fio = models.CharField(max_length=100)
-	fam = models.CharField(max_length=100)
-	name = models.CharField(max_length=100)
-	otch = models.CharField(max_length=100)
-	born = models.IntegerField()
-	dolg = models.CharField(max_length=100)
+	fio = models.CharField("ФИО",max_length=100)
+	fam = models.CharField("Фамилия",max_length=100)
+	name = models.CharField("Имя",max_length=100)
+	otch = models.CharField("Отчество",max_length=100)
+	born = models.IntegerField("Год. рожд.")
+	dolg = models.CharField("Должность",max_length=100)
 	def __str__(self):
 		return self.fio
 	class Meta:
@@ -14,13 +14,18 @@ class Person(models.Model):
     # Methods
 	def get_absolute_url(self):
 		return reverse('model-detail-view', args=[str(self.id)])
-
+	class Meta:
+		verbose_name = "Преподаватель"
+		verbose_name_plural = "Преподаватели"
+		
+		
 class Rasp(models.Model):
 	#fio = models.CharField(max_length=100)
 	#fam = models.CharField(max_length=100)
-	name = models.CharField(max_length=100)
+	name = models.CharField("Название",max_length=100)
 	#otch = models.CharField(max_length=100)
-	idpers = models.IntegerField()
+	#idpers = models.IntegerField()
+	idpers = models.ForeignKey(Person, on_delete=models.CASCADE)
 	#dolg = models.CharField(max_length=100)
 	def __str__(self):
 		return self.name
