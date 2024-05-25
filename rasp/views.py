29efea81473603f0e5affe30230cb9e95e7580f2
stanislaumich <staslz42stas.com>
2024-05-25 10:22:33 +0300

    AUTO FROM HOME 25.05.2024 10:22:32,75

--- rasp/views.py
+++ rasp/views.py
@@ -66,11 +66,6 @@ def datefromiso(year, week, day):
 def detailRaspPers(request, id, wd):
     t = id
     g = Rasp.objects.filter(idpers=t, dt__week=wd).order_by("dt", "idpara_id")
-    # print(t)
-    # print(wd)
-    # print(g[0].dt)
-    # print(datefromiso(2024, 21, 1))
-    # print( date.today().year)
     if not g:
         r = Person.objects.get(id=t)
         #dtb + timedelta(-1 * dtb.weekday() + 0)
@@ -81,7 +76,6 @@ def detailRaspPers(request, id, wd):
         np = 1
         cntx = {"r": r, "wdn": wd + 1, "wdp": wd - 1, "idp": id, "fio": r.fio, "wd": wd, "np":1, "dt":dt}
         return render(request, 'rasp/404pers.html', cntx)
-
     k = 0
     w = []
     dtb = datefromiso(date.today().year, wd, 1).date()
@@ -133,39 +127,51 @@ def detailRaspPers(request, id, wd):
                   context=cntx)
 def detailRaspAud(request, id, wd):
     t = id
-    #r = Rasp.objects.filter(idaud=t, dt__week=wd).order_by("dt", "idpara_id")
-
-    #####
+    g = Rasp.objects.filter(idpers=t, dt__week=wd).order_by("dt", "idpara_id")
+    if not g:
+        r = Aud.objects.get(id=t)
+        # dtb + timedelta(-1 * dtb.weekday() + 0)
+        # dt = (wd*7)/30 datetime.date(year=2020, month=1, day=4)
+        d = date.today() + timedelta(7)
+        d = d + timedelta(-1 * d.weekday())
+        dt = d.strftime("%Y-%m-%d")  # datetime.strptime(d, '%Y-%m-%d').date()
+        np = 1
+        cntx = {"r": r, "wdn": wd + 1, "wdp": wd - 1, "idp": id, "fio": r.name, "wd": wd, "np": 1, "dt": dt}
+        return render(request, 'rasp/404aud.html', cntx)
     k = 0
     w = []
-    dtb = datetime.today()
+    dtb = datefromiso(date.today().year, wd, 1).date()
     dtb = dtb + timedelta(-1 * dtb.weekday() + 0)
     for i in range(6):
         for j in range(7):
             try:
-                r = Rasp.objects.get(dt=dtb, idpara=j - 1, idpers=1)
-                w.append(r)
+                r = Rasp.objects.get(dt=dtb, idpara=j + 1, idpers=t)
+                w.append({'v': 1, 'i': r, "np": j})
             except:
-                w.append(0)
+                w.append({'v': 0, 'i': Para.objects.get(id=j + 1), "np": j + 1})
             k = k + 1
-            print(dtb)
+            # print(w)
         dtb = dtb + timedelta(1)
-    ######
-    if not r:
-        r = Aud.objects.get(id=t)
-        cntx = {"r": r, "wdn": wd + 1, "wdp": wd - 1, "idp": id, "fio": r.fio}
-        return render(request, 'rasp/404aud.html', cntx)
-    dtb = r.first().dt
-    cntx = {"r": r, "wdn": wd + 1, "wdp": wd - 1, "i": t,
+    # if g:
+    #     dtb = g.first().dt
+    # else:
+    #     dtb = dtb + timedelta(-1 * dtb.weekday() + 0)
+    cntx = {"r": w, "wdn": wd + 1, "wdp": wd - 1, "i": t, "wd": wd,
             "dt1": 'Понедельник,  ' + (dtb + timedelta(-1 * dtb.weekday() + 0)).strftime("%B %d "),
             "dt2": 'Вторник,  ' + (dtb + timedelta(-1 * dtb.weekday() + 1)).strftime("%B %d "),
             "dt3": 'Среда,  ' + (dtb + timedelta(-1 * dtb.weekday() + 2)).strftime("%B %d "),
             "dt4": 'Четверг,  ' + (dtb + timedelta(-1 * dtb.weekday() + 3)).strftime("%B %d "),
             "dt5": 'Пятница,  ' + (dtb + timedelta(-1 * dtb.weekday() + 4)).strftime("%B %d "),
             "dt6": 'Суббота,  ' + (dtb + timedelta(-1 * dtb.weekday() + 5)).strftime("%B %d "),
-            "r1": r[:7], "r2": r[7:14], "r3": r[14:21],
-            "r4": r[21:28], "r5": r[28:35], "r6": r[35:42],
-            "aud": r[0].idaud.name, "idp": r[0].idpers.id,
+            "d1": (dtb + timedelta(-1 * dtb.weekday() + 0)).strftime("%Y-%m-%d"),
+            "d2": (dtb + timedelta(-1 * dtb.weekday() + 1)).strftime("%Y-%m-%d"),
+            "d3": (dtb + timedelta(-1 * dtb.weekday() + 2)).strftime("%Y-%m-%d"),
+            "d4": (dtb + timedelta(-1 * dtb.weekday() + 3)).strftime("%Y-%m-%d"),
+            "d5": (dtb + timedelta(-1 * dtb.weekday() + 4)).strftime("%Y-%m-%d"),
+            "d6": (dtb + timedelta(-1 * dtb.weekday() + 5)).strftime("%Y-%m-%d"),
+            "r1": w[:7], "r2": w[7:14], "r3": w[14:21],
+            "r4": w[21:28], "r5": w[28:35], "r6": w[35:42],
+            "fio": g[0].idaud.name, "idp": 1, "wd": wd,
             "light1": 'text-light' if (dtb + timedelta(-1 * dtb.weekday() + 0)).strftime(
                 "%B %d ") == datetime.today().strftime("%B %d ") else '',
             "light2": 'text-light' if (dtb + timedelta(-1 * dtb.weekday() + 0)).strftime(
@@ -191,7 +197,7 @@ def detailRaspAud(request, id, wd):
             "bg6": 'bg-primary' if (dtb + timedelta(-1 * dtb.weekday() + 5)).strftime(
                 "%B %d ") == datetime.today().strftime("%B %d ") else '',
             }
-    return render(request, "rasp/detailRaspPers.html",
+    return render(request, "rasp/detailRaspAud.html",
                   context=cntx)
 def addRaspPers(request, id):
     res = ""
