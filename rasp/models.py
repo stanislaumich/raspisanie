f27f837c37df5bc23f8b88127c78e170b1f92ab8
stanislaumich <staslz42stas.com>
2024-06-03 22:40:06 +0300

    AUTO FROM HOME 03.06.2024 22:40:06,23

--- rasp/models.py
+++ rasp/models.py
@@ -107,11 +107,21 @@ class Rasp(models.Model):
 
 
 class Mess(models.Model):
+    CHOICES = [
+        ("0", "ОБЫЧНОЕ"),
+        ("1", "ВАЖНОЕ"),
+    ]
+
     dt = models.DateTimeField("Дата",default=datetime.now)#models.DateField("Дата", null=True)
     short = models.CharField("Кратко", max_length=100)
     long = models.CharField("Полно", max_length=255)
     isActive = models.IntegerField("Активно", default=1)
-    warn = models.IntegerField("Важно", default=0)
+    # warn = models.IntegerField("Важно", default=0)
+    warn = models.CharField(
+        max_length=1,
+        choices=CHOICES,
+        default=0,
+    )
     fromid = models.ForeignKey(Person, related_name='frompers', verbose_name="От кого", on_delete=models.PROTECT, default=0)
     toid = models.ForeignKey(Person,  related_name='topers', verbose_name="Кому", on_delete=models.PROTECT, default=0)
     def __str__(self):
