38b6676b5bb0395ce2bd30682171c1b2b8dc5cde
stanislaumich <staslz42stas@gmail.com>
2024-06-04 08:16:59 +0300

    AUTO FROM work 04.06.2024  8:16:59,46

--- mess/urls.py
+++ mess/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 
-from .import views
+from . import views
 
 urlpatterns = [
-# rasp for mess
+    # rasp for mess
     path("clear/<int:id>/", views.messClear, name="messclear"),
     path("send/<int:id>/", views.messSend, name="messsend"),
 ]
--- rasp/urls.py
+++ rasp/urls.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
 
-from .import views
+from . import views
 
 urlpatterns = [
-#common
+    #common
     path('predmet/', views.indexPredmet, name="predmetindex"),
     path("predmet/<int:id>/", views.detailPredmet, name="predmetdetail"),
     path("person/", views.indexPerson, name="personindex"),
     path("person/<int:id>/", views.detailPerson, name="persondetail"),
-	path("aud/", views.indexAud, name="audindex"),
+    path("aud/", views.indexAud, name="audindex"),
     path("aud/<int:id>/", views.detailAud, name="auddetail"),
     path("grp/", views.indexGrp, name="grpindex"),
     path("grp/<int:id>/", views.detailGrp, name="grpdetail"),
     path("rasp/", views.indexRasp, name="rspindex"),
     path("rasp/<int:id>/", views.detailRasp, name="rspdetail"),
 
-# rasp for person
+    # rasp for person
     path("rasp/person/<int:id>/<int:wd>/", views.detailRaspPers, name="rspperson"),
     path("rasp/person/add/<int:id>/", views.addRaspPers, name="rspadd"),
     path("rasp/person/edit/<int:id>/", views.editRaspPers, name="rspedit"),
@@ -24,20 +24,20 @@ urlpatterns = [
     path("rasp/person/listadd/", views.listAdd, name="rsplistadd"),
     path("rasp/person/listdel/<int:id>/", views.listDel, name="rsplistdel"),
 
-# rasp for groups
+    # rasp for groups
     path("rasp/group/<int:id>/<int:wd>/", views.detailRaspGroup, name="rspgroup"),
     path("rasp/group/add/<int:id>/", views.addRaspGroup, name="rspaddgroup"),
     path("rasp/group/edit/<int:id>/", views.editRaspGroup, name="rspeditgroup"),
     path("rasp/group/del/<int:id>/", views.delRaspGroup, name="rspdelgroup"),
 
-# rasp for aud
+    # rasp for aud
     path("rasp/aud/<int:id>/<int:wd>/", views.detailRaspAud, name="rspaud"),
 
-# tests
+    # tests
     path("rasp/gen/", views.genRaspPers, name="rspgen"),
     path("rasp/test/", views.test, name="rsptest"),
 
-# auth
+    # auth
     path("login/", views.login, name="login"),
 
 ]
