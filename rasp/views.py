2170dfdedd0d6100450bbafb4226943c6ae27799
stanislaumich <staslz42stas@gmail.com>
2024-06-04 16:15:47 +0300

    AUTO FROM work 04.06.2024 16:15:47,46

--- rasp/views.py
+++ rasp/views.py
@@ -32,7 +32,7 @@ def detailPredmet(request, id):
 
 def indexPerson(request):
     week = request.session.get('week', datetime.today().isocalendar()[1])
-    people = MyPers.objects.filter(myid=request.session['userid']).all()
+    people = MyPers.objects.filter(myid=request.session.get('userid',1)).all()
     # print(people)
     # wd = datetime.today().isocalendar()[1]  request.session['week'] = 'mini'
     #wd = week
