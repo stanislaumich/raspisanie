7ae4ab665d27f45f874f0a900ecdd9963dec5691
stanislaumich <staslz42stas@gmail.com>
2024-06-27 16:19:43 +0300

    AUTO FROM work 27.06.2024 16:19:43,72

--- raspisanie/views.py
+++ raspisanie/views.py
@@ -18,8 +18,9 @@ def index(request):
     mes = Mess.objects.filter(toid=uid, isActive=1).all().order_by('-id')
     mesmy = Mess.objects.filter(fromid=uid, isActive=1).all().order_by('-id')
     messys = Alert.objects.filter(toid=uid, isActive=1).all().order_by('-id')
-    data = {'a': 'fio', 'mes': mes, 'mesmy': mesmy, 'messys':messys}
+    data = {'a': 'fio', 'mes': mes, 'mesmy': mesmy, 'messys': messys}
     return render(request, "index.html", context=data)
+
 
 # ---------------------------------
 
