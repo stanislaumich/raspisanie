9e0a8884f02ad5610843e1281337eb53c1eaa147
stanislaumich <staslz42stas@gmail.com>
2024-06-27 08:28:58 +0300

    AUTO FROM work 27.06.2024  8:28:58,53

--- person/serializers.py
+++ person/serializers.py
@@ -2,12 +2,6 @@ from rest_framework import serializers
 from .models import Person
 
 
-# class StudentSerializer(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = Student
-#         fields = ('pk', 'name', 'email', 'document', 'phone', 'registrationDate','photo')
-
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
